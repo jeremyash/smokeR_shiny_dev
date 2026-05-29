a873654fde49fe32f1721e8eac78328758124c0e
jeremyash <jeremyd.ash@gmail.com>
2026-05-29 14:43:36 -0400

    setting up function to deploy app

--- smokeR_dev/deploy_app.R
+++ smokeR_dev/deploy_app.R
@@ -1,3 +1,9 @@
-rsconnect::deployApp(appDir = "smokeR_dev",
-                     appTitle = "smoker",
-                     account = "r8-arm")
+deploy_prod <- function() {
+  rsconnect::deployApp(
+    appDir = "smokeR_dev",
+    appName = "smoker",
+    account = "r8-arm"
+  )
+}
+
+deploy_prod()
